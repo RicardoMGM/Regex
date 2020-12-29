11873aac2aa7b1a229f5899baa63d97ac9212b27
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2020-12-29 17:28:10 -0300

    Update UsandoChaves.js

--- src/quantificadores/UsandoChaves.js
+++ src/quantificadores/UsandoChaves.js
@@ -1,10 +1,15 @@
 const texto = 'O João recebeu 120 milhões apostando 6 9 21 23 45 46.'
 
 // para definir uma quantificador usa {}
+// Pega com um ou dois dígitos
 console.log(texto.match(/\d{1,2}/g))
+// Pega só os dígitos de 2 posições
 console.log(texto.match(/[0-9]{2}/g))
+// Como não definiu o valór após a vírgula ele vai pegar o máximo de caractéres possível
 console.log(texto.match(/\d{1,}/g))
 
+// Pega com exatamente 7 digitos
+// \w -> trabalha com os grupos do short hand
 console.log(texto.match(/\w{7}/g))
 console.log(texto.match(/[\wõã]{7,}/g))
 
