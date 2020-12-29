d75d6f5b870489b81b183a9e8b43f75868084dc9
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2020-12-29 18:58:15 -0300

    Update DesafioTelefone.js

--- src/quantificadores/DesafioTelefone.js
+++ src/quantificadores/DesafioTelefone.js
@@ -5,5 +5,8 @@ Lista telefônica:
     - (85) 99988-7766
     - (21)3261-8899
 `
-
+// () -> são meta caracteres por isso tem que usar o scape
+// Os parenteses são opcionais por isso tem que utilizar o metacaracter "?"
+// O ddd após o ")" é opcional por isso no espaço "\s" utilizo o "?" para informar que é opcional
+// Os dígitos dentro dos "()" vai de 0 até 2 porque o ddd é pcional
 console.log(texto.match(/\(?\d{0,2}\)?\s?\d{4,5}-\d{4}/g))
