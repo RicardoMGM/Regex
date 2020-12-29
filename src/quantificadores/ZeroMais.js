6f830414140171029b2097f132ea3b131db988f0
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2020-12-29 17:18:25 -0300

    Update ZeroMais.js

--- src/quantificadores/ZeroMais.js
+++ src/quantificadores/ZeroMais.js
@@ -1,6 +1,7 @@
 const texto1 = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOO!'
 const texto2 = 'There is a big fog in NYC'
 
+// Pega 0 ou mais ocorrências do o
 // * -> zero ou mais
 const regex = /fogo*/gi
 console.log(texto1.match(regex))
