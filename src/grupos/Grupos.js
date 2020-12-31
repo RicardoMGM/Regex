9b88ac59c86643020c8f203e3f00532604d635c9
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2020-12-30 21:15:34 -0300

    Update Grupos.js

--- src/grupos/Grupos.js
+++ src/grupos/Grupos.js
@@ -1,5 +1,8 @@
 const texto1 = 'O José Simão é muito engraçado... hehehehehehe'
+
+// + -> Pega um ou mais "he"
 console.log(texto1.match(/(he)+/g))
 
+// ? -> opcional
 const texto2 = 'http://www.site.info www.escola.ninja.br google.com.ag'
 console.log(texto2.match(/(http:\/\/)?(www\.)?\w+\.\w{2,}(\.\w{2})?/g))
