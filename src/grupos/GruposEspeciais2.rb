8be3e6439b054228acb97e93082656489422a49f
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 12:20:16 -0300

    Update GruposEspeciais2.rb

--- src/grupos/GruposEspeciais2.rb
+++ src/grupos/GruposEspeciais2.rb
@@ -1,3 +1,6 @@
+
+# Não é suportado pelo JavaScript por isso foi feito em Hugby
+# Look behind
 texto = 'supermercado superação hiperMERCADO Mercado'
 
 puts texto.scan(/(?:super)[\wÀ-ú]+/i).join(' ')
