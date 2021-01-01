51e4c5e5ea47090df0524554912c884288fec0f2
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 14:57:55 -0300

    Update Dotall.js

--- src/bordas/Dotall.js
+++ src/bordas/Dotall.js
@@ -1,4 +1,6 @@
 const texto = 'Romário era um excelente jogador\n, mas hoje é um político questionador'
 
 console.log(texto.match(/^r.*r$/gi)) // problema do dotall
+// Resolve o problema no caso de ter um "\n" na string.
+// "\s\S" -> Tudo que é espaço e não espaço.
 console.log(texto.match(/^r[\s\S]*r$/gi))
