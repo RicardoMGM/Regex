acaf654a2dd8cb4ad04158a21af96fc407a714f9
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 12:03:38 -0300

    Update GruposAlgunsCuidados.js

--- src/grupos/GruposAlgunsCuidados.js
+++ src/grupos/GruposAlgunsCuidados.js
@@ -1,5 +1,8 @@
 const texto = 'Pedrinho (filho do Pedro Silva) é doutor do ABCabc!'
 
+// Dentro de um conjunto "[]", o grupo não existe "()"
 console.log(texto.match(/[(abc)]/gi)) // não é um grupo
+// O conjunto já existe dentro de um grupo
 console.log(texto.match(/([abc])/gi))
+// Evite de usar grupo de forma desnecessário no caso só para pegar a sequencia abc, não precisava criar um grupo.
 console.log(texto.match(/(abc)+/gi))
