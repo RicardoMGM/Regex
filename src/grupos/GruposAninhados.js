3fdfa9eb7cba80aafa9559d2704c1ab5d1a3f88b
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 11:58:42 -0300

    Update GruposAninhados.js

--- src/grupos/GruposAninhados.js
+++ src/grupos/GruposAninhados.js
@@ -1,4 +1,6 @@
 const texto = 'supermercado hipermercado minimercado mercado'
 
+// | -> Significa OU . Como tem o "?" depois do grupo esse grupo é opcional
 console.log(texto.match(/(super|hiper|mini)?mercado/g))
+// A mesda da de cima só para não precisar repetir a parte "per"
 console.log(texto.match(/((hi|su)per|mini)?mercado/g))
