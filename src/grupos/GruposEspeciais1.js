43d04adc781eb36b28f5296a76c36d89f0ae19b8
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 12:13:24 -0300

    Update GruposEspeciais1.js

--- src/grupos/GruposEspeciais1.js
+++ src/grupos/GruposEspeciais1.js
@@ -1,12 +1,19 @@
 const texto = 'João é calmo, mas no transito fica nervoso.'
 
+// À-ú -> pega todas as letras acentuadas
 console.log(texto.match(/[\wÀ-ú]+/gi))
 
 // Positive lookahead
+// +(?=,) -> pega a palavra que tem uma "," na frente
 console.log(texto.match(/[\wÀ-ú]+(?=,)/gi))
+// +(?=\.) -> pega a palavra que tem um "." na frente
 console.log(texto.match(/[\wÀ-ú]+(?=\.)/gi))
+// +(?=, mas) -> pega a palavra que tem um "mas" na frente
 console.log(texto.match(/[\wÀ-ú]+(?=, mas)/gi))
 
 // Negative lookahead
+// \b -> Borda
+// (?!,) -> paralvras que não estejam na frente de ","
 console.log(texto.match(/[\wÀ-ú]+\b(?!,)/gi))
+// "[\s|\.]" Que tenha um espaço ou ponto depois da palavra
 console.log(texto.match(/[\wÀ-ú]+[\s|\.](?!,)/gi))
