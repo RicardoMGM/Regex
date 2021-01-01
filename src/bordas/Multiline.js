e48de4707bad202c4bac37f4a2a39b5494cc478a
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 15:03:24 -0300

    Update Multiline.js

--- src/bordas/Multiline.js
+++ src/bordas/Multiline.js
@@ -3,7 +3,9 @@ Leo é muito legal
 Emanuel foi jogar em Sergipe
 Bianca é casada com Habib
 `
-
+// Mostra quantas quebra de linhas tem a string
 console.log(texto.match(/\n/g))
+// Mostra o caracter que começa e termina cada frase, porem não funciona porque o ".+" não pega o "\n" (quebra de linha)
 console.log(texto.match(/^(\w).+\1$/gi))
+// m -> no final, vai considerar cada linha de forma independente.
 console.log(texto.match(/^(\w).+\1$/gim))
