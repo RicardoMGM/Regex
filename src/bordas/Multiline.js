6c4443591d051b314ca55dfad775ad43b8ff257f
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-01 15:04:57 -0300

    Update Multiline.js

--- src/bordas/Multiline.js
+++ src/bordas/Multiline.js
@@ -7,5 +7,5 @@ Bianca é casada com Habib
 console.log(texto.match(/\n/g))
 // Mostra o caracter que começa e termina cada frase, porem não funciona porque o ".+" não pega o "\n" (quebra de linha)
 console.log(texto.match(/^(\w).+\1$/gi))
-// m -> no final, vai considerar cada linha de forma independente.
+// m -> no final, vai considerar cada linha de forma independente. Quando quer trabalhar com bordas e pegar tudo da linha trabalhe com "m"
 console.log(texto.match(/^(\w).+\1$/gim))
