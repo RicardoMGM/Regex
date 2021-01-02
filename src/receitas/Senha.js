f3cce8452cfc64ad204c6e0aafdff40b8e1b92d2
Ricardo Manuel <35776304+RicardoMGM@users.noreply.github.com>
2021-01-02 11:17:58 -0300

    Update Senha.js

--- src/receitas/Senha.js
+++ src/receitas/Senha.js
@@ -8,7 +8,9 @@ QUASE123!
 #OpA1?
 Foi123!
 `
-
+// Qualquer caracter entre 6 e 20 caracteres
 console.log(texto.match(/^.{6,20}$/gm))
+// Verifica se temabém tem alguma letras maiúsculas entre A - Z
 console.log(texto.match(/^(?=.*[A-Z]).{6,20}$/gm))
+// Adiciona um segundo grupo de letras minúsculas, verifica também se tem dígitos, e verifica se tem caracteres especiais.
 console.log(texto.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%!^&*]).{6,20}$/gm))
